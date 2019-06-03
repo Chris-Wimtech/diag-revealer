000c14d7de19be114455e85fc59f21bf43587c48
Chris-Wimtech <39478134+Chris-Wimtech@users.noreply.github.com>
2019-06-03 13:15:48 +0200

    Update usage.sh

--- usage.sh
+++ usage.sh
@@ -1,5 +1,5 @@
 ./diag_revealer
-    DIAG_CFG                path to a config file specifying the types of messages to recieve. see /example.cfg
+    DIAG_CFG                path to a config file specifying the types of messages to recieve. see https://github.com/Chris-Wimtech/mobileinsight-core/tree/reverse_engineering/dm_collector_c
     FIFI_PATH               path to the fi/fo pipe used to communicate between diage_revealer and APP code.
     [LOG_OUTPUT_DIR]        optional path to a directory to dump logs.
     [LOG_CUT_SIZE (in MB)]  optional maximum size at which to slice logs. No logs larger than this will be produced.
