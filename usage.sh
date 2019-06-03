ddd27031a8ea5d0a9b030934465f742099907452
Christopher Albers <christopher@wimtechnologies.com>
2019-06-03 13:04:38 +0200

    Add some usage stuff

---
+++ usage.sh
@@ -0,0 +1,5 @@
+./diag_revealer
+    DIAG_CFG                path to a config file specifying the types of messages to recieve. see /example.cfg
+    FIFI_PATH               path to the fi/fo pipe used to communicate between diage_revealer and APP code.
+    [LOG_OUTPUT_DIR]        optional path to a directory to dump logs.
+    [LOG_CUT_SIZE (in MB)]  optional maximum size at which to slice logs. No logs larger than this will be produced.
